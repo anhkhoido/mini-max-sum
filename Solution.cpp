436681f57505848cae9e6a63918cd0686f3c50ca
Anh Khoi Do <anhkhoido@gmail.com>
2017-06-20 15:38:05 -0400

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -3,19 +3,31 @@
 #include <new>
 #include <valarray>
 
+// The definitive size of the pointer array of positive integers.
 #define SIZE 5
 
 void input(long long arr[]);
 
+//
+// The main function of the program.
 int main() {
     
+    // The pointer array that will store the five positive integers.
     long long* arr;
     
+    // Allocation of the memory space for the pointer array.
+    // If the pointer array is null, then the program will proceed
+    // to this function's return statement.
     arr = new (std::nothrow) long long[SIZE];
     if (arr != nullptr) {
         
         input(arr);
+        
+        // Sort the array of positive integers.
         std::sort(arr, arr + SIZE);
+        
+        // Display the smallest and biggest sums one can obtain by adding
+        // a combination of four integers of his/her choice in the array.
         std::cout   << (std::valarray<long long>(arr, SIZE).sum() - arr[4]) << " " 
                     << (std::valarray<long long>(arr, SIZE).sum() - arr[0]) << std::endl;
     }
@@ -27,6 +39,10 @@ int main() {
     return 0;
 }
 
+//
+// This function allows the user to input five integers to the array
+// that is passed as an argument. The array can only accept positive integers
+// inclusively between 1 and 10 to the power of 9.
 void input(long long arr[]) {
     
     long long temp;
